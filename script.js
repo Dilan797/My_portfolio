584c413d0c3d34b5dd1f83c272f3515c71ce258a
Dilan797 <dilanblues@gmail.com>
2024-07-02 17:43:14 -0500

    Segundo commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+//En esta parte del código damos el efecto cundo inicia la web
+var tl = gsap.timeline()
+
+tl.to("#page1",{
+    y:"100vh",
+    scale:0.5
+})
+tl.to("#page1",{
+    y:"30vh",
+    duration: 1,
+    delay:1     
+})
+tl.to("#page1",{
+    y:"0vh",
+    rotate:360,
+    scale:1,
+    duration: 0.8,
+})
